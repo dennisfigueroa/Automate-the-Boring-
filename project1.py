430108f66d3418a05f4fc73d0db0c29836049017
dennisfigueroa <denniskfig@gmail.com>
2019-10-06 05:09:40 +0100

    Rather than using BeautifulSoup, I had to use Selenium to dynamically scrape because I needed to wait for the Javascript to load in the page before I was able to see the information.

---
+++ project1.py
@@ -0,0 +1,34 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+#Dynamically scraping requires opening up Chrome because the JS has to load to enter the HTML to be scrapped
+
+
+#Opens up my local webdriver and launches Chrome
+browser = webdriver.Chrome("/Users/dennisfigueroa/Downloads/chromedriver")
+browser.maximize_window()
+time.sleep(3)
+website_url = 'https://champion.gg/statistics/#?sortBy=general.winPercent&order=descend&roleSort=ADC'
+browser.get(website_url)
+
+#This deals with the Cookie Acceptance in the initial screen
+browser.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/button[2]").click()
+time.sleep(2)
+
+#Code here to grab the top 5 Champions by Winrate
+
+#Print the name of the 5
+
+#Convert this to JS and add it to Discord Bot
+
+
+
+
+
+
+
+
